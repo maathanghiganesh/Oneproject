43e4ee87b89509899bda993d8cfb56a559682f7e
maathanghiganesh <maathanghiganesh@gmail.com>
2020-09-06 13:00:52 +0530

    To A

---
+++ src/org/t/Sample.java
@@ -0,0 +1,30 @@
+package org.t;
+
+import java.util.Arrays;
+
+public class Sample {
+	public static void main(String[] args) {
+		String s1="silent";
+		String s2="listen";
+		
+		if(s1.length()==s2.length()){
+			char[] c1 = s1.toCharArray();
+			char[] c2 = s2.toCharArray();
+			
+			Arrays.sort(c1);
+			Arrays.sort(c2);
+			
+			if(Arrays.equals(c1, c2)) {
+				System.out.println("it is anagram");
+			}
+			else {
+				System.out.println("not anagram");
+			}
+		}
+		
+		
+	}
+
+}
+
+
